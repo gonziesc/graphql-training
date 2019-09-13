49e4980d589f599e3244df888b97fa7281de8162
gonza <gonziesc@gmail.com>
2019-09-13 10:56:29 -0300

    finishing filter

--- app/graphql/products/resolvers.js
+++ app/graphql/products/resolvers.js
@@ -3,9 +3,8 @@ const { mapProducts } = require('../../serializers/products');
 
 const getProducts = (_, { page, limit, filter }) => {
   const offset = (page - 1) * limit;
-  return getProductsFromDb({ limit, offset, filter }).then(products =>
-    mapProducts({ products, page, limit })
-  );
+  const where = filter ? { where: filter } : {};
+  return getProductsFromDb({ limit, offset, where }).then(products => mapProducts({ products, page, limit }));
 };
 
 module.exports = {
--- app/services/products.js
+++ app/services/products.js
@@ -1,6 +1,6 @@
 const { Product, Variant } = require('../models');
 
-const getProducts = ({ limit, offset, filter }) =>
-  Product.findAndCountAll({ include: [{ model: Variant, as: 'variants' }], limit, offset, filter });
+const getProducts = ({ limit, offset, where }) =>
+  Product.findAndCountAll({ include: [{ model: Variant, as: 'variants' }], limit, offset, ...where });
 
 module.exports = { getProducts };
