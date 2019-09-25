7a5eb3b80bb0fc72c8f588aa9546c554e9eecb10
Lucas <lucas.medrano@wolox.com.ar>
2019-09-25 15:30:15 -0300

    add like filters

--- app/graphql/products/resolvers.js
+++ app/graphql/products/resolvers.js
@@ -1,20 +1,27 @@
 const { getProducts: getProductsFromDb } = require('../../services/products');
-const { getVariants: getVariantsFromDb } = require('../../services/variants');
 const { mapProducts } = require('../../serializers/products');
+const Sequelize = require('sequelize');
 
 const getProducts = (_, { page, limit, filter }) => {
   const offset = (page - 1) * limit;
-  const where = filter ? { where: filter } : {};
+  const where = { where: {} };
+
+  for (const filterField in filter.where) {
+    if (filterField) {
+      where.where[filterField] = filter.where[filterField];
+    }
+  }
+
+  for (const filterField in filter.like) {
+    if (filterField) {
+      where.where[filterField] = { [Sequelize.Op.substring]: filter.like[filterField] };
+    }
+  }
   return getProductsFromDb({ limit, offset, where }).then(products => mapProducts({ products, page, limit }));
 };
-
-const getVariants = ({ id: productId }) => getVariantsFromDb({ productId });
 
 module.exports = {
   Query: {
     products: getProducts
-  },
-  Product: {
-    variants: getVariants
   }
 };
